2dbfde0e4aa95f22c80d1f948399a0140a925ba5
Ash <82991315+ashutoshbhadouria@users.noreply.github.com>
2026-04-05 17:15:41 -0400

    vault backup: 2026-04-05 17:15:40

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "ashutosh bhadouria",
+    pageTitleSuffix: "ashu",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
